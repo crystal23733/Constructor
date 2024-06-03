f3cffa6e5c89d8e3d12835c2bd1b6c5542b481e2
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-03 11:40:47 +0900

    object만듦

--- work-1.js
+++ work-1.js
@@ -2,6 +2,15 @@
 // * 목적은 함수를 조금 더 이해하려고 함.
 // * 매개변수가 객체인 경우가 아래에 작성될 예정임.
 
-function workOne (a, b) {
-  return 오브젝트;
+function workOne (a,b) {
+  // todo 지역변수로 객체를 초기화, 매개변수 a,b두 개가 객체의 키이름과 값 리터럴이 되었음 좋겠음.
+  let localValue = {};
+  localValue[a] = a;
+  localValue[b] = b;
+  // for(let i = 0; i < arg[i]; i++){
+  //   localValue[i] = arg;
+  // }
+  return localValue;
 }
+
+console.log(workOne('name','hoyeong'));
