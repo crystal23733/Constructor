944c6354194ef1c3cc315a4ab1092f9281b83c43
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-03 15:37:01 +0900

    class

--- old.js
+++ old.js
@@ -1,3 +1,19 @@
 function old(a, b) {
-  
+  this.first = a;
+  this.second = b;
 }
+
+let test = new old(1,2);
+console.log(test);
+
+
+
+class NewOld {
+  constructor(a, b){
+    this.first = a;
+    this.second = b;
+  }
+}
+
+let test2 = new NewOld(1, 2);
+console.log(test2);
